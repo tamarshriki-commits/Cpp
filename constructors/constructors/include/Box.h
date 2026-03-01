1f5b6c0ec360ba827da35b767bd4015342e211f7
tamarshriki <tamar.shriki@gmail.com>
2026-03-01 16:28:06 +0200

    fixing cr

--- constructors/constructors/include/Box.h
+++ constructors/constructors/include/Box.h
@@ -7,10 +7,10 @@ class Box
 public:
 	Box() = default;
 	Box(int weight, const std::string& name);
-	int getWeight() const;
-	const std::string& getName() const;
-	void setWeight(int weight);
-	void setName(const std::string& name);
+	int getWeight() const { return weight_; }
+	std::string getName() const { return name_; }
+	void setWeight(int weight) { weight_ = weight; }
+	void setName(const std::string& name) { name_ = name; }
 private:
 	int weight_;
 	std::string name_;
--- constructors/constructors/src/Box.cpp
+++ constructors/constructors/src/Box.cpp
@@ -4,19 +4,3 @@ Box::Box(int weight, const std::string& name) {
 	weight_ = weight;
 	name_ = std::string(name);
 }
-
-int Box::getWeight() const {
-	return weight_;
-}
-
-const std::string& Box::getName() const {
-	return name_;
-}
-
-void Box::setWeight(int weight) {
-	weight_ = weight;
-}
-
-void Box::setName(const std::string& name) {
-	name_ = name;
-}
